3e12d3454ecddc554e11977fad9c9f7b82b1f932
Dawid Nowicki <dawid.nowicki@alsoft.pl>
2019-03-18 23:09:03 +0100

    Remove code of sdk

--- sdk/src/main/java/com/pledg/sdk/PledgSDK.kt
+++
@@ -1,9 +0,0 @@
-package com.pledg.sdk
-
-import com.pledg.sdk.model.Transaction
-
-
-interface PledgSDK{
-    val viewModel: PledgSDKViewModel
-    fun startTransaction(transaction: Transaction)
-}
--- sdk/src/main/java/com/pledg/sdk/PledgSDKFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.pledg.sdk
-
-import android.app.Activity
-import android.webkit.WebView
-
-fun createPlegSDK(activity: Activity, web: WebView): PledgSDK =
-    PledgSDKImp(activity,web)
